6545b65ebfcd28e6a755e5eb863b3a86038dc8ff
Luke Sandberg <lukeisandberg@gmail.com>
2010-10-31 17:35:15 -0500

    some simple extension methods for code printing

--- analysis/Extensions.cs
+++ analysis/Extensions.cs
@@ -17,7 +17,7 @@ namespace FaultLocalization
 		/// <param name="ds">The Coverage Data Set that this line is based in</param>
 		/// <param name="line">The line we are interested in</param>
 		/// <returns>The source code around the given line</returns>
-		public static String GetCoveredCode(this CoverageDS ds, CoverageDS.LinesRow line)
+		public static String GetCoveredLine(this CoverageDS ds, CoverageDS.LinesRow line)
 		{
 			uint file_id = line.SourceFileID;
 			String filename = ds.SourceFileNames.Where(fn => fn.SourceFileID == file_id).Select(f => f.SourceFileName).First();
@@ -55,7 +55,7 @@ namespace FaultLocalization
 		/// </summary>
 		/// <param name="reader">The Stream to read</param>
 		/// <returns>The lines in the file or resource</returns>
-		public static IEnumerable<String> Lines(this StreamReader reader)
+		public static IEnumerable<String> Lines(this TextReader reader)
 		{
 			String line;
 			while((line = reader.ReadLine()) != null)
