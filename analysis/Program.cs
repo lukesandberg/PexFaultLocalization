c4d729ef4829a25724733615f55d88f20c65735a
Luke Sandberg <lukeisandberg@gmail.com>
2010-12-04 15:18:01 -0600

    missed one

--- analysis/Program.cs
+++ analysis/Program.cs
@@ -168,46 +168,4 @@ namespace FaultLocalization
 			Environment.Exit(1);
 		}
 	}
-
-	public class SastatementSuspiciousnessInfo
-	{
-		public SourceCodeLocation Id { get; private set; }
-
-		public int Passed
-		{
-			get
-			{
-				return Tests.Where(t => t.Result).Count();
-			}
-		}
-
-		public int Failed
-		{
-			get
-			{
-				return Tests.Where(t => !t.Result).Count();
-			}
-
-		}
-
-		public IList<ExecutedTest> Tests { get; private set; }
-
-		public IDictionary<ISuspiciousnessRater, float> SuspiciousnessRatings
-		{
-			get;
-			private set;
-		}
-
-		public DynamicBasicBlock DBB { get; set; }
-
-		public StatementSuspiciousnessInfo(SourceCodeLocation id)
-		{
-			this.Id = id;
-			Tests = new List<ExecutedTest>();
-			SuspiciousnessRatings = new Dictionary<ISuspiciousnessRater, float>();
-		}
-
-
-
-	}
 }
