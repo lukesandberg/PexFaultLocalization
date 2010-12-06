d29cd664c8dfa46a97e6dc290ae3cdce0ef5eac8
ryano144 <ryano144@gmail.com>
2010-12-05 19:48:29 -0600

    Moved TotInfo and Replace to SiemensSuite folder
    Signed-off-by: ryano144 <ryano144@gmail.com>

---
+++ SiemensSuite/Replace/Replace.Tests/Edu/Nlu/Sir/Siemens/Replace/ReplaceVersionFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edu.Nlu.Sir.Siemens.Replace;
+using Edu.Nlu.Sir.Siemens.Shared;
+
+namespace Edu.Nlu.Sir.Siemens.Replace
+{
+    internal class ReplaceVersionFactory
+    {
+        public static IReplace getReplaceVersion()
+        {
+            Environment.SetEnvironmentVariable("Version", "1");
+            string VersionString = Environment.GetEnvironmentVariable("Version");
+             int VersionNum;
+            bool numeric = Int32.TryParse(VersionString, out VersionNum);
+            if (numeric)
+            {
+                var x = typeof(IReplace).TypesImplementingInterface(true);
+                x = x.Where(t => t.Name.Contains(VersionString));
+                x = x.Where(t => false);
+                string VersionName = "Edu.Nlu.Sir.Siemens.Replace.Version" + VersionString;
+                IReplace Version = (IReplace)Type.GetType(VersionName).GetConstructor(Type.EmptyTypes).Invoke(new object[0]);
+                return Version;
+            }
+            else
+            {
+                return new BaseVersion();
+            }
+        }
+
+        
+
+    }
+}
---
+++ SiemensSuite/Shared/FaultyVersion.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+
+namespace Edu.Nlu.Sir.Siemens.Shared
+{
+    public enum FaultType
+    {
+        NO_FAULT,
+        MISSING_CODE, 
+        ADDED_CODE,
+        NUMERIC_CHANGE,
+        OPERATOR_CHANGE,
+        IF_CONDITION_CHANGE,
+        IF_OPERATOR_CHANGE,
+        CONSTANT_VALUE_CHANGE,
+        FORLOOP_PARAMETER_CHANGE,
+        ARRAY_INDEX_CHANGE
+    }
+
+    public interface FaultyVersion
+    {
+        int[] FaultLines { get; }
+        FaultType FaultType { get; }
+    }
+
+    public static partial class Extensions {
+        public static bool ReadLine(this TextReader reader, out char[] result, int maxsize)
+        {
+            StringBuilder s = new StringBuilder();
+            while (s.Length < maxsize && reader.Peek() != -1 && reader.Peek() != '\n')
+            {
+                s.Append(reader.Read());
+            }
+            if(reader.Peek() != -1) {
+                s.Append(reader.Read());
+            }
+            if (s.Length == 0)
+            {
+                result = null;
+                return false;
+            }
+            else
+            {
+                result = s.ToString().ToCharArray();
+                return true;
+            }
+        }
+
+        /// <summary> 
+        /// Returns all types in the current AppDomain implementing the interface or inheriting the type.  
+        /// </summary> 
+        /// <param name="onlyRealTypes">if true, only real, instantiable types will be returned</param>
+        public static IEnumerable<Type> TypesImplementingInterface(this Type interfaceType, bool onlyRealTypes)
+        {
+             var x = AppDomain
+                .CurrentDomain
+                .GetAssemblies();
+             List<Type> types = new List<Type>();
+             foreach (Assembly assembly in x)
+             {
+                 try
+                 {
+                     types.AddRange(assembly.GetTypes().AsEnumerable());
+                 }
+                 catch (Exception e)
+                 {
+
+                 }
+             }
+             return types.Where(type => interfaceType.IsAssignableFrom(type) && (!onlyRealTypes || type.IsRealClass()));
+        }
+
+        /// <param name="testType">The type to test</param>
+        /// <returns>true if this type is non-abstract, non-generic, and non-interface</returns>
+        public static bool IsRealClass(this Type testType)
+        {
+            return testType.IsAbstract == false
+                && testType.IsGenericTypeDefinition == false
+                && testType.IsInterface == false;
+        }
+
+    }
+}
