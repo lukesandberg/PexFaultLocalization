3fe2b928d043c5bf593ac41cd31a27c00e54bafd
ryano144 <ryano144@gmail.com>
2010-12-06 00:26:06 -0600

    Signed-off-by: ryano144 <ryano144@gmail.com>

---
+++ SiemensSuite/Replace/Replace.Tests/Properties/PexAssemblyInfo.cs
@@ -0,0 +1,30 @@
+// <copyright file="PexAssemblyInfo.cs" company="General Electric">Copyright © General Electric 2010</copyright>
+using Microsoft.Pex.Framework.Coverage;
+using Microsoft.Pex.Framework.Creatable;
+using Microsoft.Pex.Framework.Instrumentation;
+using Microsoft.Pex.Framework.Moles;
+using Microsoft.Pex.Framework.Settings;
+using Microsoft.Pex.Framework.Validation;
+
+// Microsoft.Pex.Framework.Settings
+[assembly: PexAssemblySettings(TestFramework = "VisualStudioUnitTest")]
+
+// Microsoft.Pex.Framework.Instrumentation
+[assembly: PexAssemblyUnderTest("Replace")]
+[assembly: PexInstrumentAssembly("Shared")]
+
+// Microsoft.Pex.Framework.Creatable
+[assembly: PexCreatableFactoryForDelegates]
+
+// Microsoft.Pex.Framework.Validation
+[assembly: PexAllowedContractRequiresFailureAtTypeUnderTestSurface]
+[assembly: PexAllowedXmlDocumentedException]
+
+// Microsoft.Pex.Framework.Coverage
+[assembly: PexCoverageFilterAssembly(PexCoverageDomain.UserOrTestCode, "Shared")]
+
+// Microsoft.Pex.Framework.Moles
+[assembly: PexAssumeContractEnsuresFailureAtBehavedSurface]
+[assembly: PexChooseAsBehavedCurrentBehavior]
+
+[assembly: PexInstrumentType("mscorlib", "System.IO.TextReader+SyncTextReader")]
